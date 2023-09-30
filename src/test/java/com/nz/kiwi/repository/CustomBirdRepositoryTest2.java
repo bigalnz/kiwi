0ab092565427c92f72394374d62050989fed113f
AlGrant <bigal.nz@gmail.com>
2023-09-30 15:59:54 +1300

    Bird Controller cleaned up Basic methods working with Custom Repo

--- src/test/java/com/nz/kiwi/repository/CustomBirdRepositoryTest2.java
+++ src/test/java/com/nz/kiwi/repository/CustomBirdRepositoryTest2.java
@@ -1,12 +1,5 @@
 package com.nz.kiwi.repository;
 
-import com.nz.kiwi.SexFaker;
-import com.nz.kiwi.StatusFaker;
-import com.nz.kiwi.TaxaFaker;
-import com.nz.kiwi.model.Bird;
-import jakarta.persistence.EntityManager;
-import net.datafaker.Faker;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +8,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.sql.DataSource;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -29,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
         "spring.test.database.replace=none",
         "spring.datasource.url=jdbc:tc:mysql:8.1.0:///test?TC_REUSABLE=true"
 })
-public class CustomBirdRepositoryTest {
+public class CustomBirdRepositoryTest2 {
 
     @Autowired
     private TestEntityManager entityManager;
@@ -37,6 +24,12 @@ public class CustomBirdRepositoryTest {
     @Autowired
     private CustomBirdRepository customBirdRepository;
 
+    @TestConfiguration
+    @Import(CustomBirdRepository.class)
+    static class TestConfig {
+    } // must a static class.
+
+    //...
     @Test
     void shouldReturnReturnContosoDetalisDto() {
     }
